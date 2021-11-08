530cf9126d25879804a0140518309ca09073ca1b
vUnAmp <phanhaingoc@gmail.com>
2021-11-08 11:26:06 +0100

    handle dashboard

--- functions/src/handlers/booking/seeding.ts
+++ functions/src/handlers/booking/seeding.ts
@@ -6,10 +6,28 @@ import gaoBookings from "./gaobookings.json"
 
 export const seedingBookings = (req: Request, res: Response) => {
   gaoBookings.map(booking => {
+    const {
+      status,
+      first_name: firstName,
+      last_name: lastName,
+      selectedDate,
+      selectedSlot,
+      email,
+      phone,
+      require,
+      _id,
+    } = booking || {}
     db.collection("gao-vegan0410940")
-      .doc()
+      .doc(_id.$oid)
       .set({
-        ...booking,
+        status,
+        selectedDate,
+        selectedSlot,
+        email,
+        phone,
+        require,
+        firstName,
+        lastName,
         terminAt: dayjs(booking?.selectedDate).unix() / 3600,
       })
       .then(() => {})
